6c0c09046526ad8dd7f72091d52fa6341ede8b97
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2024-07-24 07:58:25 +0330

    feat: Implement `single project` view and add `google font family`.

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="project">
+    <div @click="showDetails = !showDetails" class="actions">
+      <h3>{{ project.title }}</h3>
+      <div class="icons">
+        <span class="material-symbols-outlined">delete</span>
+        <span class="material-symbols-outlined">edit</span>
+        <span class="material-symbols-outlined">check</span>
+      </div>
+    </div>
+    <div v-if="showDetails" class="details">
+      <p>{{ project.details }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'project'
+  ],
+  data() {
+    return {
+      showDetails: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.project {
+  margin: 20px auto;
+  background: white;
+  padding: 10px 20px;
+  border-radius: 4px;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.5);
+  border-left: 4px solid #e90074;
+}
+
+h3 {
+  cursor: pointer;
+}
+
+.actions {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.material-symbols-outlined {
+  font-size: 24px;
+  margin-left: 10px;
+  color: #bbb;
+  cursor: pointer;
+}
+
+.material-symbols-outlined:hover {
+  color: #777;
+}
+
+</style>
