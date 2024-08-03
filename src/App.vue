8d44f8c9bc6ede65c9bfd042de9528c62090ec3d
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2024-08-03 11:40:49 +0330

    feat: Implement `navbar`.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
+  <navbar />
   <router-view/>
 </template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  components: {Navbar}
+}
+</script>
 
 <style>
 body {
